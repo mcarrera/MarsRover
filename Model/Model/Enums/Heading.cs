6d8bc1a9a6ba92b204baf03887c2a7b78cdb2b70
mcarrera <mcarrera00@gmail.com>
2020-03-19 16:32:23 -0400

    compass image

--- Model/Model/Enums/Heading.cs
+++ Model/Model/Enums/Heading.cs
@@ -2,11 +2,17 @@
 
 namespace Domain.Model.Enums
 {
-  public enum Heading
-  {
-    [Description("N")] North = 0,
-    [Description("E")] East = 90,
-    [Description("S")] South = 180,
-    [Description("W")] West = 270,
-  }
+    public enum Heading
+    {
+        [Description("N")] North = 0,
+        [Description("E")] East = 90,
+        [Description("S")] South = 180,
+        [Description("W")] West = 270,
+
+        // for demo purposes only
+        NortEast = 45,
+        SouthEast = 135,
+        SouthWest = 225,
+        NorthWest = 315,
+    }
 }
